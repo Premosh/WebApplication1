3bd7d6d58ab01333c667ebb5b37f472af8d8c71e
LOQ <LOQ@LAPTOP-6TLAOJ90>
2025-08-14 21:35:13 +0545

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Create(StudentDetails st)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Details",st);
+            }
+            return View();
+        }
+        public IActionResult Details(StudentDetails st)
+        {
+            return View(st);
+        }
+    }
+}
---
+++ Models/StudentDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class StudentDetails
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Phone is required")]
+        public string Phone { get; set; }
+    }
+}
